0e4112a2923f7cc5e3d2aa78655910921fcfb9de
fahraz <fahrazfirdaus@gmail.com>
2025-02-05 22:02:41 +0700

    implementasi rendering list

--- src/components/Fragments/CardProducts.jsx
+++ src/components/Fragments/CardProducts.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button/button";
 const CardProducts = (props) => {
     const {children} = props;
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
             {children}
         </div>
     )
@@ -21,11 +21,11 @@ const header = (props) => {
 }
 
 const body = (props) => {
-    const {children, title} = props
+    const {children, name} = props
     return(
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
-                <h5 className="text-xl font-semibold tracking-tight text-white">{title}</h5>
+                <h5 className="text-xl font-semibold tracking-tight text-white">{name}</h5>
                 <p className="text-s text-white">{children}</p>
             </a>
         </div>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,17 +1,39 @@
 import CardProduct from "../components/Fragments/CardProducts.jsx";
+
+const products = [
+    {
+        id:1,
+        name: "Nike Air Max 90",
+        price: "Rp. 1.000.000",
+        image:"/public/images/shoes.jpg",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illum dolorum tenetur hic tempore ab excepturi, mollitia perspiciatis est eum debitis quia aut. Illo dignissimos porro accusantium ab repellat nobis?"
+    },
+    {
+        id:2,
+        name: "Calvien Sports",
+        price: "Rp. 1.500.000",
+        image:"/public/images/shoes2.jpg",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illum dolorum tenetur hic tempore ab excepturi."
+    },
+    {
+        id:3,
+        name: "Sepatu adadong",
+        price: "Rp. 2.000.000",
+        image:"/public/images/shoes3.jpg",
+        description:"Sepatu adadong nih boys ready kuyy dichek out."
+    },
+]
 const ProductPage = ()=> {
     return(
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.header image="/public/images/shoes.jpg"></CardProduct.header>
-                <CardProduct.body title="Nike Air Max 90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illum dolorum tenetur hic tempore ab excepturi, mollitia perspiciatis est eum debitis quia aut. Illo dignissimos porro accusantium ab repellat nobis?</CardProduct.body>
-                <CardProduct.footer price="Rp. 1.500.000"></CardProduct.footer>
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.header image="/public/images/shoes.jpg"></CardProduct.header>
-                <CardProduct.body title="Nike Air Max 90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illum dolorum tenetur hic tempore ab excepturi, mollitia perspiciatis est eum debitis quia aut. Illo dignissimos porro accusantium ab repellat nobis?</CardProduct.body>
-                <CardProduct.footer price="Rp. 1.500.000"></CardProduct.footer>
-            </CardProduct>
+            {products.map((product) => (
+             <CardProduct>
+                <CardProduct.header image= {product.image}></CardProduct.header>
+                <CardProduct.body name={product.name}>{product.description}</CardProduct.body>
+                <CardProduct.footer price={product.price}></CardProduct.footer>
+             </CardProduct>
+            ))}
+
         </div>
     )
 }
